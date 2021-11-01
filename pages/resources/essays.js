ab8369e3fa1c570eb785fa72d3217ad02851edea
Alan hu <chessplayer292@gmail.com>
2021-11-01 02:13:04 -0400

    fixed import getSession issue

--- pages/resources/essays.js
+++ pages/resources/essays.js
@@ -14,6 +14,7 @@ import {getSession} from  "next-auth/client"
 import axios from "axios"
 import { Tabs, TabList, TabPanels, Tab, TabPanel } from "@chakra-ui/react"
 import Essay from "../../components/essay/essay";
+import { getSession } from "next-auth/client";
 
 const Essays = ({ session, data }) => {
     const isSummerEssay = (essay) => {
